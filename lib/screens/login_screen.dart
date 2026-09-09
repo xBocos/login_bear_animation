66d3004f602672d9291e594d2bd6987c5fe74e01
Leonardo Bocos <LE23080289@merida.tecnm.mx>
2026-09-08 20:45:20 -0600

    feat(rive): animation added

---
+++ lib/screens/login_screen.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+import 'package:rive/rive.dart';
+
+class LoginScreen extends StatefulWidget {
+  const LoginScreen({super.key});
+
+  @override
+  State<LoginScreen> createState() => _LoginScreenState();
+}
+
+class _LoginScreenState extends State<LoginScreen> {
+  @override
+  Widget build(BuildContext context) {
+    //Para obtener el tamaño de la pantalla
+    final Size size = MediaQuery.of(context).size;
+    return Scaffold(
+      body: SafeArea(
+        child: Padding(
+          padding: EdgeInsets.symmetric(horizontal: 20),
+          child: Column(children: [
+            SizedBox(height: 300, child: RiveAnimation.asset('login-bear.riv'))
+          ]),
+        ),
+      ),
+    );
+  }
+}
